6e47d1e12f9d90efb2d8906bc32f877f48e6bfaa
Lucy <user@domain.tld>
2026-03-27 20:43:35 +0100

    src: misfits.c: start with basic initialization
    Main function lives here. Adds the initialization logic that has been
    implemented so far.
    
    Signed-off-by: Lucy <user@domain.tld>

--- src/main/misfits.c
+++ src/main/misfits.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include "driver/gptimer.h"
+#include "esp-log.h"
+#include "esp-system.h"
+
+#include "epd-spi.h"
+#include "common-defs.h"
+
 
 void app_main(void)
 {
+	int ret;
 
+	// initialize general purpose timer
+	ret = gptimer_new_timer(&timer_config, &gptimer);
+	if (ret) {
+		ESP_LOGE(TAG, "Error %d: Couldn't create timer instance\n", ret);
+		goto err;
+	}
+
+	ret = gptimer_enable(gptimer);
+	if (ret) {
+		ESP_LOGE(TAG, "Error %d: Couldn't enable timer\n", ret);
+		goto err;
+	}
+
+	ret = gptimer_start();
+	if (ret) {
+		ESP_LOGE(TAG, "Error %d: Couldn't start timer\n", ret);
+		goto err;
+	}
+
+
+	// initialize ePaper display
+	ret = epd_gpio_init();
+	if (ret) {
+		ESP_LOGE(TAG, "Error %d: Couldn't initialize eDP display\n", ret);
+		goto err;
+	}
+
+	/* alarm function (basically a callback) to change the time displayed on
+	*  epaper display
+	*/
+
+
+err:
+	ESP_LOGI(TAG, "performing restart now...\n");
+	esp_restart();
 }
