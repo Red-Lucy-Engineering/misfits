0fc65242f5ee245aa6d05905314ebb9c98e348c0
Lucy <user@domain.tld>
2026-03-29 17:40:02 +0200

    src: epd-spi/watch-timer: change static to static inline
    Signed-off-by: Lucy <user@domain.tld>

--- src/main/include/watch-timer.h
+++ src/main/include/watch-timer.h
@@ -15,8 +15,24 @@ extern gptimer_event_callbacks_t cbs;
 esp_err_t timer_init();
 
 
-static bool perminute_timer_on_alarm_cb(gptimer_handle_t timer,
+
+// TODO: Implement proper way of drawing graphics to the screen first
+static inline bool perminute_timer_on_alarm_cb(gptimer_handle_t timer,
 					const gptimer_alarm_event_data_t *edata,
-					void *user_ctx);
+					void *user_ctx) {
+	// ...
+	/* General process for handling event callbacks:
+	 * 1. Retrieve user context data from user_ctx (passed in from
+	 *    gptimer_register_event_callbacks)
+	 * 2. Get alarm event data from edata, such as edata->count_value
+	 * 3. Perform user-defined operations
+	 * 4. Return whether a high-priority task was awakened during the
+	 *    above operations to notify the scheduler to switch tasks
+
+	 *    Reference: https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/gptimer.html#triggering-periodic-alarm-events
+	 */
+
+	return false;
+}
 
 #endif //_WATCH_TIMER_H
--- src/main/watch-timer.c
+++ src/main/watch-timer.c
@@ -59,21 +59,3 @@ esp_err_t timer_init() {
 	return ret;
 }
 
-// TODO: Implement proper way of drawing graphics to the screen first
-static bool perminute_timer_on_alarm_cb(gptimer_handle_t timer,
-					const gptimer_alarm_event_data_t *edata,
-					void *user_ctx) {
-	// ...
-	/* General process for handling event callbacks:
-	 * 1. Retrieve user context data from user_ctx (passed in from
-	 *    gptimer_register_event_callbacks)
-	 * 2. Get alarm event data from edata, such as edata->count_value
-	 * 3. Perform user-defined operations
-	 * 4. Return whether a high-priority task was awakened during the
-	 *    above operations to notify the scheduler to switch tasks
-
-	 *    Reference: https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/gptimer.html#triggering-periodic-alarm-events
-	 */
-
-	return false;
-}
